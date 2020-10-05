73017d9209cf355f0dbbcd7acaf219bb886bd50b
Dominik Szarkiel <d.szarkiel@gmail.com>
2020-10-05 17:56:39 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -9,3 +9,11 @@ def loop_message_n_times(string, count)
     puts string
   end
 end
+
+def output_array(array)
+  counter = 0
+  while array[coutner] do
+    puts array[counter]
+    counter += 1
+  end
+end
